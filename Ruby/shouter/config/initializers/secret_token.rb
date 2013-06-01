b7ec73f52cdb7e2b0f2a1658a103ad68f5ea68ad
Eric Lee <ehwl229@gmail.com>
2013-06-01 15:05:55 -0400

    Creating Shouter - Twitter clone using Ruby on Rails

---
+++ Ruby/shouter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = '3b41a17fdedb168ae8c7b1569cfc146cad4873a780160bd6f205f2e26dce41af6cadf93b8c3b47305db7dd54c2abb4fca46c18a963395cffac3a36924e32e89f'
