bebf91a6da447831e9123f7afacef0a96c4f81bc
Eric Lee <ehwl229@gmail.com>
2013-06-18 00:04:52 -0400

    Shouther with User Authentication

---
+++ Ruby/shouter_auth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = '5e800cef2b3cdbadee2633e1e663fefdcbbe61d7942ed35f4c1b78bf4d1e1d08908d62af8ffc688d84fc40f7df4848d061a0a0b33981d79e122acc611909939a'
